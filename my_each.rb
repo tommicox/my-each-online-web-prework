67d34a093ecf3fb69a5ae2d876f552e5ec30a291
Tommi Cox <tommicox@gmail.com>
2019-03-06 20:10:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(collection) 
   i = 0
-while i < collection.length  
-until(collection[i])
-i = i + 1
+  while i < collection.length  
+    yield(collection[i])
+    i = i + 1
  end
-collection
+ collection
 end
